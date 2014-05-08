cc6f504389ceb9a8c128cbe38429a819585cd90b
carlospalairi <carlos@paliari.com.br>
2014-05-08 18:35:24 -0300

    add prioridades do produtos

---
+++ entity-files/Produtos.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Class Produtos
+ *
+ * @Entity
+ * @Table(name="produtos")
+ */
+class Produtos extends AbstractModel
+{
+    /**
+     * Primary Key column.
+     *
+     * @Id
+     * @GeneratedValue
+     * @Column(type="integer")
+     */
+    protected $id;
+
+    /**
+     * Data de cadastro
+     * @Column(type="datetime")
+     */
+    protected $datcad;
+
+    /**
+     * @Column(type="string", length=100, nullable=true)
+     */
+    protected $descricao;
+
+    /**
+     * @Column(type="decimal")
+     */
+    protected $preco;
+
+    /**
+     * @Column(type="integer")
+     */
+    protected $grupo_id;
+
+    /**
+     * @ManyToOne(targetEntity="Grupos")
+     * @JoinColumn(name="grupo_id", referencedColumnName="id")
+     */
+    private $grupo;
+
+}   
